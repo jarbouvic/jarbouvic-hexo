7baffe927ae759ff88f1104f5582d6e71c830aec
Victor <jarbouvic@gmail.com>
2016-11-18 13:08:08 +0700

    "test"

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -5,39 +5,21 @@ var exec = require('child_process').exec;
 
 
 gulp.task('git-add', function(cb) {
-  exec('git add --all .', function(err) {
-    if (err) return cb(err); // return error
-    cb(); // finished task
-  });
+  run('git add --all .').exec('', cb)
+  .pipe(gulp.dest('output'));
 });
 
 gulp.task('git-commit', ['git-add'], function(cb) {
-  exec('git commit -m "test"', function(err) {
-    if (err) return cb(err); // return error
-    cb(); // finished task
-  });
+  run('git commit -m "test"').exec('', cb)
+  .pipe(gulp.dest('output'));
 });
 
 gulp.task('git-push', ['git-commit'], function(cb) {
-  exec('ssh-agent bash -c "ssh-add C:\Users\Victor\.ssh\id_rsa; git push origin master"', function(err) {
+  run('git push origin master', function(err) {
     if (err) return cb(err); // return error
     cb(); // finished task
   });
 });
-
-function git_add(msg) {
-    return gulp.src('.')
-        .pipe(git.add())
-        .pipe(git.commit(msg))
-        .pipe(git.push('origin', 'master', function (err) {
-            if (err) throw err;
-        }));
-};
-
-function git_run(msg) {
-    return run('echo').exec(msg, callback);
-};
-
 
 gulp.task('deploy', ['git-push'], function() {
     process.chdir('hugo/public');
