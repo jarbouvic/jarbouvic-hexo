cc77bf4259fa3a072d03016ef219c6021015b40e
Victor <jarbouvic@gmail.com>
2016-11-18 14:25:22 +0700

    test

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -5,7 +5,9 @@ var exec = require('child_process').exec;
 
 gulp.task('git-push', function(cb) {
     var msg= "test";
-    exec('git add --all . & git commit -m ' + msg + ' & git push origin master', function(err, stdin, stdout) {
+    exec('git add --all . & git commit -m ' + msg + ' & git push origin master', function(err, stdout, stderr) {
+        gutil.log(stdout);
+        gutil.log(stderr);
         cb(err);
     });
 });
