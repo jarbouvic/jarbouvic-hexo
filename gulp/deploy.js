8246525862b70c0e3c4e3878254b48660a0e57f2
Victor <jarbouvic@gmail.com>
2016-11-21 11:17:21 +0700

    'rebuilding+site+2016-11-21'

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -1,57 +1,28 @@
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
+var argv = require('yargs').argv;
+var shell = require('./tools/shellHelper');
 
-
-// spawn a child process and execute shell command
-// borrowed from https://github.com/mout/mout/ build script
-// author Miller Medeiros
-// released under MIT License
-// version: 0.1.0 (2013/02/01)
-
-
-// execute a single shell command where "cmd" is a string
-var exec = function(cmd, cb){
-    // this would be way easier on a shell/bash script :P
-    var child_process = require('child_process');
-    var parts = cmd.split(/\s+/g);
-    console.log('$'+cmd);
-    var p = child_process.spawn(parts[0], parts.slice(1), {stdio: 'inherit'});
-    p.on('exit', function(code){
-        console.log('$'+cmd+' --- END');
-        var err = null;
-        if (code) {
-            err = new Error('command "'+ cmd +'" exited with wrong status code "'+ code +'"');
-            err.code = code;
-            err.cmd = cmd;
-        }
-        if (cb) cb(err);
-    });
-};
-
-
-// execute multiple commands in series
-// this could be replaced by any flow control lib
-var series = function(cmds, cb){
-    var execNext = function(){
-        exec(cmds.shift(), function(err){   
-            if (err) {
-                cb(err);
-            } else {
-                if (cmds.length) execNext();
-                else cb(null);
-            }
-        });
-    };
-    execNext();
-};
-
-
-gulp.task('git:push', function(cb) {
-    var msg= "test";
-    
-    series([
+function git_push(cb) {
+    var today= new Date().toISOString().slice(0, 10);
+    var msg= (argv.msg) ? argv.msg : 'rebuilding\ site\ '+today;
+    msg = msg.split(' ').join('+');
+    shell.series([
         'git add --all .',
-        'git commit -m "'+ msg +'"',
+        "git commit -m '"+ msg +"'",
         'git push origin master'
     ], cb);
+}
 
+gulp.task('git:push', function(cb) {
+    git_push(cb);
+});
+
+gulp.task('git:publish', function(cb) {
+    process.chdir('hugo/public');
+    git_push(cb);
+});
+
+gulp.task('deploy', function(cb) {
+    runSequence('hugo:public','git:push', 'git:publish',cb);
 });
--- gulp/hugo.js
+++ gulp/hugo.js
@@ -1,36 +1,24 @@
 var gulp = require('gulp');
-var spawn = require('child_process').exec;
-var gutil = require('gulp-util');
+var shell = require('./tools/shellHelper');
 var path = require('path');
 var del = require('del');
 
-function hugo(publish) {
+function hugo(publish, cb) {
     var src = path.join(process.cwd(), 'hugo');
     var dst = path.join(process.cwd(), 'hugo/public');
-
-    gutil.log('src: ' + src);
 
     var cmd = 'hugo'; 
     cmd += (publish) ? ' -d ' + dst : ' server';
     cmd += ' -t perso -s '+ src;
     cmd += (publish) ? '' : ' --buildDrafts --verbose';
 
-    var child = spawn(cmd, {encoding: 'utf-8'});
-
-    child.stdout.on('data', function (data) {
-        gutil.log('hugo: \n' + data);
-    });
-
-    child.stderr.on('data', function (data) {
-        gutil.log('hugo: \n' + data);
-    });
-
+    shell.exec(cmd, cb)
 }
 
-gulp.task('hugo:public', ['build'], function() {
-    hugo(true);
+gulp.task('hugo:public', ['build'], function(cb) {
+    hugo(true, cb);
 });
 
-gulp.task('hugo:serve', ['build'], function() {
-    hugo(false);
+gulp.task('hugo:serve', ['build'], function(cb) {
+    hugo(false, cb);
 });
